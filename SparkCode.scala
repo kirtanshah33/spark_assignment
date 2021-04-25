1c9dbe9335652e6ba7cc6e7d9634d40c5004ce04
kirtanshah33 <59151394+kirtanshah33@users.noreply.github.com>
2021-04-25 19:29:44 +0530

    modified code with checkpointing and related fixes

--- SparkCode.scala
+++ SparkCode.scala
@@ -1,18 +1,25 @@
-  package org.example
+package org.example
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, concat, concat_ws, lit, split, unix_timestamp}
-import org.apache.spark.sql.types.{DoubleType, TimestampType}
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType, TimestampType}
 
 object SparkCode extends App {
-  Logger.getLogger("org").setLevel(Level.WARN)
-  Logger.getLogger("akka").setLevel(Level.WARN)
+  val input = args(0)
+  val output = args(1)
   val spark = SparkSession.builder()
     .master("local[1]")
     .appName("spark-app")
     .getOrCreate();
-  val inputFile = spark.readStream.json("//input_folder")
+  val schema = StructType(
+    List(
+      StructField("message", StringType, nullable = true),
+    )
+  )
+
+  val inputFile = spark.readStream.schema(schema).json(input)
   val filteredMessage = inputFile.filter(col("message").contains("omwssu"))
   val parsedMessage = filteredMessage.withColumn("fields", split(col("message"), " +"))
 
@@ -37,7 +44,7 @@ object SparkCode extends App {
     withColumn("cpe_id",cpe_id_field).
     withColumn("error_code",error_code).drop(col("url")).drop(col("url_fields"))
 
-  parseUrl.writeStream.format("json").option("path","output/").
-    outputMode("append").
+  parseUrl.writeStream.format("json").option("checkpointLocation","\\home\\checkpoints\\").option("path",output).
+    outputMode("append").trigger(Trigger.ProcessingTime("10 seconds")).
     start().awaitTermination()
 }
